4e5e2c343201d201181418488eaddc2fad3c8266
Usofl <90086886+Usofl@users.noreply.github.com>
2022-04-22 21:29:29 +0900

    220422_++조심

--- Team_1/Team_1/Maingame.cpp
+++ Team_1/Team_1/Maingame.cpp
@@ -40,7 +40,7 @@ void CMaingame::Update(void)
 
 	// ÃÑ¾Ë
 	for (std::list<CObj*>::iterator iter = m_BulletList.begin();
-		iter != m_BulletList.end();)
+		iter != m_BulletList.end(); ++iter)
 	{
 		(*iter)->Update();
 	}
@@ -53,7 +53,7 @@ void CMaingame::Late_Update(void)
 
 	// ÃÑ¾Ë
 	for (std::list<CObj*>::iterator iter = m_BulletList.begin();
-		iter != m_BulletList.end();)
+		iter != m_BulletList.end(); ++iter)
 	{
 		(*iter)->Late_Update();
 	}
@@ -87,7 +87,7 @@ void CMaingame::Render(void)
 
 	// ÃÑ¾Ë
 	for (std::list<CObj*>::iterator iter = m_BulletList.begin();
-		iter != m_BulletList.end();)
+		iter != m_BulletList.end(); ++iter)
 	{
 		(*iter)->Render(m_hDC);
 	}
