521e1975964b3f374bc5f11a7db02938914f9c13
손준혁 <@DESKTOP-DJPQ98N>
2022-04-23 16:51:16 +0900

    플레이어 벽 충돌 수정

--- Team_1/Team_1/Player.cpp
+++ Team_1/Team_1/Player.cpp
@@ -83,7 +83,11 @@ void CPlayer::Pick_Up_Item(CObj * _Item)
 
 void CPlayer::Key_Input(void)
 {
-	if (GetAsyncKeyState(VK_LEFT))
+	RECT rc{};
+	RECT LeftWall{ GAMESIZE - 10, GAMESIZE, GAMESIZE, WINCY - GAMESIZE };
+	RECT RightWall{ WINCX - GAMESIZE, GAMESIZE, WINCX - GAMESIZE + 10, WINCY - GAMESIZE };
+
+	if (GetAsyncKeyState(VK_LEFT) && (!IntersectRect(&rc, &LeftWall, &m_tRC)))
 	{
 		if (GetAsyncKeyState(VK_UP))
 		{
@@ -100,7 +104,7 @@ void CPlayer::Key_Input(void)
 			m_tInfo.fX -= m_fSpeed;
 		}
 	}
-	else if (GetAsyncKeyState(VK_RIGHT))
+	else if (GetAsyncKeyState(VK_RIGHT) && (!IntersectRect(&rc, &RightWall, &m_tRC)))
 	{
 		if (GetAsyncKeyState(VK_UP))
 		{
@@ -159,25 +163,21 @@ void CPlayer::Key_Input(void)
 void CPlayer::Collision_Wall(void)
 {
 	RECT rc{};
-	RECT LeftWall{ GAMESIZE - 10, GAMESIZE, GAMESIZE, WINCY - GAMESIZE };
-	RECT RightWall{ WINCX - GAMESIZE, GAMESIZE, WINCX - GAMESIZE + 10, WINCY - GAMESIZE };
 	RECT TopWall{ GAMESIZE, GAMESIZE - 10, WINCX - GAMESIZE, GAMESIZE };
 	RECT BottomWall{ GAMESIZE, WINCY - GAMESIZE, WINCX - GAMESIZE, WINCY - GAMESIZE + 10 };
 
-	if (IntersectRect(&rc, &LeftWall, &m_tRC))
-	{
-		--m_iHP;
-	}
-	if (IntersectRect(&rc, &RightWall, &m_tRC))
-	{
-		--m_iHP;
-	}
 	if (IntersectRect(&rc, &TopWall, &m_tRC))
 	{
-		--m_iHP;
+		m_tInfo.fY = (WINCY - GAMESIZE) - (PLAYERCY / 2.f);
+
+		m_tPoint.x = (long)(m_tInfo.fX + m_fBSize * cosf(m_fAngle * DEGREE));
+		m_tPoint.y = (long)(m_tInfo.fY - m_fBSize * sinf(m_fAngle * DEGREE));
 	}
 	if (IntersectRect(&rc, &BottomWall, &m_tRC))
 	{
-		--m_iHP;
+		m_tInfo.fY = (GAMESIZE)+(PLAYERCY / 2.f);
+
+		m_tPoint.x = (long)(m_tInfo.fX + m_fBSize * cosf(m_fAngle * DEGREE));
+		m_tPoint.y = (long)(m_tInfo.fY - m_fBSize * sinf(m_fAngle * DEGREE));
 	}
 }
