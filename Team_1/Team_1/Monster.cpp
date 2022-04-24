fc704b196a26c8b919c6ddfc3f6107b2d4343497
Usofl <90086886+Usofl@users.noreply.github.com>
2022-04-24 12:34:24 +0900

    220424_몬스터 A타입 그리기

--- Team_1/Team_1/Monster.cpp
+++ Team_1/Team_1/Monster.cpp
@@ -24,6 +24,8 @@ void CMonster::Initialize(void)
 
 	m_iHP = 2;
 
+	m_iReverse = 1;
+
 	m_fSpeed = 10.f;
 }
 
@@ -62,19 +64,45 @@ void CMonster::Update(void)
 
 void CMonster::Late_Update(void)
 {
+	if (m_pPlayer->Get_fX() - m_tInfo.fX > 0)
+	{
+		LONG temp = m_tRC.left;
+		m_tRC.left = m_tRC.right;
+		m_tRC.right = temp;
+
+		m_iReverse = -1;
+	}
+	else
+	{
+		m_iReverse = 1;
+	}
 }
 
 void CMonster::Render(HDC _hDC)
 {
 	if (MONSTERTYPE_A == m_MonType)
 	{
-		MoveToEx(_hDC, m_tRC.left - 10, m_tRC.top + 10, nullptr);
-		LineTo(_hDC, m_tRC.left + 15, m_tRC.top + 10);
-		LineTo(_hDC, m_tRC.right - 5, m_tRC.top - 10);
-		LineTo(_hDC, m_tRC.right - 5, m_tRC.bottom + 10);
-		LineTo(_hDC, m_tRC.left + 15, m_tRC.bottom - 10);
-		LineTo(_hDC, m_tRC.left - 10, m_tRC.bottom - 10);
-		LineTo(_hDC, m_tRC.left - 10, m_tRC.top + 10);
+
+		Ellipse(_hDC, m_tRC.left + (15 * m_iReverse), m_tRC.top + 10, m_tRC.right - (5 * m_iReverse), m_tRC.bottom - 10);
+
+		MoveToEx(_hDC, m_tRC.left, m_tRC.top + 10, nullptr);
+		LineTo(_hDC, m_tRC.left + (10 * m_iReverse), m_tRC.top + 10);
+		LineTo(_hDC, m_tRC.left + (10 * m_iReverse), m_tRC.top - 7);
+		LineTo(_hDC, m_tRC.left + (15 * m_iReverse), m_tRC.top - 7);
+		LineTo(_hDC, m_tRC.left + (15 * m_iReverse), m_tRC.top + 10);
+
+		LineTo(_hDC, m_tRC.right + (5 * m_iReverse), m_tRC.top + 10);
+		LineTo(_hDC, m_tRC.right + (10 * m_iReverse), m_tRC.top + 5);
+		LineTo(_hDC, m_tRC.right + (10 * m_iReverse), m_tRC.bottom - 5);
+		LineTo(_hDC, m_tRC.right + (5 * m_iReverse), m_tRC.bottom - 10);
+
+		LineTo(_hDC, m_tRC.left + (15 * m_iReverse), m_tRC.bottom - 10);
+		LineTo(_hDC, m_tRC.left + (15 * m_iReverse), m_tRC.bottom + 7);
+		LineTo(_hDC, m_tRC.left + (10 * m_iReverse), m_tRC.bottom + 7);
+		LineTo(_hDC, m_tRC.left + (10 * m_iReverse), m_tRC.bottom - 10);
+		LineTo(_hDC, m_tRC.left, m_tRC.bottom - 10);
+		LineTo(_hDC, m_tRC.left, m_tRC.top + 10);
+
 	}
 
 	else if (MONSTERTYPE_B == m_MonType)
