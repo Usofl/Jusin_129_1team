5b3a4ecfaa9edbe267f7e79fffdc0b1a966c5658
Usofl <90086886+Usofl@users.noreply.github.com>
2022-04-24 15:28:11 +0900

    .

--- Team_1/Team_1/stdafx.h
+++ Team_1/Team_1/stdafx.h
@@ -19,11 +19,10 @@
 
 
 // TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
-
 #include <list>
 #include <vector>
 #include <ctime>//시간
 #include <cmath>//수학공식 cos sin abs sqrt등등
 #include "Include.h"
 
-
+extern HWND g_hWnd;
